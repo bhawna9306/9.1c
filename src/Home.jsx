d04482eaa3c3a29b0aefc0130a07b20b51247fd5
Bhawna <bhawna4775.be22@chitkara.edu.in>
2023-10-12 12:20:07 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Header from "./Header";
+import deakin from "./1.png"
+import Cardlist from "./CardList";
+import Cardlist2 from "./CardList2";
+import Ender from "./Ender";
+import Contact from "./Contact";
+import { Link } from 'react-router-dom';
+
+const imgstyle = {
+    width: "100%",
+    height: "600px"
+}
+
+
+const welcomeStyle = {
+    fontSize: "36px", // You can adjust the font size as needed
+    
+};
+
+function Home() {
+    return (
+        <div>
+            <div className="nav">
+            <Link to='/'><span><h1 style={welcomeStyle}>Home</h1></span></Link>
+               
+            </div>
+            <br></br>
+            <Header />
+            <br></br>
+            <img src={deakin} alt="deakin" style={imgstyle} />
+            <br></br>
+            <h1>Featured Articles</h1>
+            <Cardlist /><br></br>
+            <button className="Button">see all articles</button>
+            <br></br>
+            <h1>Featured Tutorials</h1>
+            <Cardlist2 /><br></br>
+            <button className="Button">see all tutorials</button>
+            <br></br>
+            <Ender />
+            <Contact />
+        </div>
+    )
+}
+
+export default Home
